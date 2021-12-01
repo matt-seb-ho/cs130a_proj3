12a649d26f9d4b5f76277767f071b73ed20e38cc
Matthew Ho <msho@ucsb.edu>
2021-12-01 01:49:51 -0800

    added output formatting

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include "graph_op.h"
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <stdexcept>
 
@@ -13,21 +12,16 @@ void readInput(GraphGenerator& gg, std::string fname) {
 		throw std::runtime_error("file open failed\n");
 	}
 	std::string line;
-	std::stringstream ss;
-	int x, y;
-	int counter = 0;
+	int x, y, commaIndex;
 	while (std::getline(in, line)) {
-		line += ',';
-		ss.str(line);
-		std::getline(ss, line, ',');
-		x = stoi(line);
-		std::getline(ss, line, ',');
-		y = stoi(line);
-		// std::cout << "x, y: " << x << ", " << y << "\n";
+		commaIndex = line.find(",");
+		x = stoi(line.substr(0, commaIndex));
+		y = stoi(line.substr(commaIndex + 1));
 		gg.insertEdge(x, y);
+
+		// debugging
+		// std::cout << "x, y: " << x << ", " << y << "\n";
 		// gg.printPreOrder();
-		counter++;
-		
 	}
 	in.close();
 }
@@ -40,34 +34,17 @@ int main(int argc,  char* argv[]) {
 	GraphGenerator gg;
 	readInput(gg, argv[1]);
 	
+	std::cout << "The in order traversal:\n";
 	gg.printInOrder();
+
+	std::cout << "The pre order traversal:\n";
 	gg.printPreOrder();
-	std::cout << GraphOperator::isAcyclic(gg) << std::endl;
+
+	std::cout << "Is acyclic(Yes/No):\n";
+	std::cout << (GraphOperator::isAcyclic(gg) ? "Yes\n" : "No\n");
+
+	std::cout << "Connected Components:\n";
 	GraphOperator::connectedComponents(gg); 
 
-	/*
-	GraphGenerator gg;
-	gg.insertVertex(3);
-	gg.insertVertex(2);
-	gg.insertVertex(1);
-	gg.insertEdge(1, 2);
-	std::cout << "connected components, 1 edge:\n";
-	GraphOperator::connectedComponents(gg);
-	std::cout << "----\n";
-	gg.insertEdge(2, 3);
-	gg.insertEdge(1, 3);
-	std::cout << "in order: ";
-	gg.printInOrder();
-	std::cout << "pre order: ";
-	gg.printPreOrder();
-
-	std::cout << GraphOperator::isAcyclic(gg) << std::endl;
-	std::cout << "connected components, 3 edges:\n";
-	GraphOperator::connectedComponents(gg);
-	std::cout << "----\n";
-	*/
-
-
-	// std::cout << "hello world" << std::endl;
 	return 0;
 }
